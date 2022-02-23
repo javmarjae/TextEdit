004ea2cf0655ecc19a6707a43d10acb71e9577de
Javier <javmarjae@gmail.com>
2022-02-23 12:41:54 +0100

    Tercera adaptación

--- textEdit/todo.py
+++ textEdit/todo.py
@@ -18,7 +18,7 @@ from utils.i18n import trans
 from utils.commands import *
 from controllers.views import ViewsController
 from controllers.file import FileController
-from controllers.textEditingTools import TextEditingTools
+from controllers.text import TextEditingTools
 from controllers.bars import ToolBars, MenuBars
 from utils.widgets.actions import Actions
 
@@ -42,12 +42,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.clipboard = QApplication.clipboard()
         self.statusBar = QStatusBar()
         self.cursor = QtGui.QTextCursor()
-        self.undoStack = QUndoStack()
-        self.undoCommand = QUndoCommand()
         self.setFocus()
         self.fileController = FileController(self)
         self.tools = TextEditingTools(self)
-        self._createActions()
+        self.createActions = Actions(self)
         self._createMenuBar()
         self._createToolBars()
 
@@ -62,26 +60,26 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         #Añadimos el menu archivo con sus acciones
         menuBar.addMenu(fileMenu)
-        fileMenu.addAction(self.newFile)
-        fileMenu.addAction(self.openFile)
-        fileMenu.addAction(self.saveFile)
+        fileMenu.addAction(self.createActions.newFile)
+        fileMenu.addAction(self.createActions.openFile)
+        fileMenu.addAction(self.createActions.saveFile)
         fileMenu.addSeparator()
-        fileMenu.addAction(self.closeApp)
+        fileMenu.addAction(self.createActions.closeApp)
 
         #Añadimos el menu editar con sus acciones
         menuBar.addMenu(editMenu)
-        editMenu.addAction(self.copyText)
-        editMenu.addAction(self.pasteText)
-        editMenu.addAction(self.cutText)
+        editMenu.addAction(self.createActions.copyText)
+        editMenu.addAction(self.createActions.pasteText)
+        editMenu.addAction(self.createActions.cutText)
         editMenu.addSeparator()
-        editMenu.addAction(self.undo)
-        editMenu.addAction(self.redo)
+        #editMenu.addAction(self.undo)
+        #editMenu.addAction(self.redo)
 
         #Añadimos el menu ayuda con sus acciones
         menuBar.addMenu(helpMenu)
-        helpMenu.addAction(self.help)
+        helpMenu.addAction(self.createActions.help)
         helpMenu.addSeparator()
-        helpMenu.addAction(self.about)
+        helpMenu.addAction(self.createActions.about)
 
         self.setMenuBar(menuBar)
 
@@ -93,107 +91,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.addToolBar(fileToolBar)
         self.addToolBar(editToolBar)
 
-        fileToolBar.addAction(self.newFile)
-        fileToolBar.addAction(self.openFile)
-        fileToolBar.addAction(self.saveFile)
+        fileToolBar.addAction(self.createActions.newFile)
+        fileToolBar.addAction(self.createActions.openFile)
+        fileToolBar.addAction(self.createActions.saveFile)
 
         fileToolBar.setMovable(False)
 
-        editToolBar.addAction(self.copyText)
-        editToolBar.addAction(self.pasteText)
-        editToolBar.addAction(self.cutText)
+        editToolBar.addAction(self.createActions.copyText)
+        editToolBar.addAction(self.createActions.pasteText)
+        editToolBar.addAction(self.createActions.cutText)
         editToolBar.addSeparator()
         self.fontSizeBox = QSpinBox()
         self.fontSizeBox.setFocusPolicy(Qt.NoFocus)
         editToolBar.addWidget(self.fontSizeBox)
-        editToolBar.addAction(self.bold)
-        editToolBar.addAction(self.italic)
+        editToolBar.addAction(self.createActions.bold)
+        editToolBar.addAction(self.createActions.italic)
         editToolBar.addSeparator()
-        editToolBar.addAction(self.header1)
-        editToolBar.addAction(self.header2)
-        editToolBar.addAction(self.header3)
+        editToolBar.addAction(self.createActions.header1)
+        editToolBar.addAction(self.createActions.header2)
+        editToolBar.addAction(self.createActions.header3)
         editToolBar.addSeparator()
-        editToolBar.addWidget(self.undoButton)
-        editToolBar.addWidget(self.redoButton)
+        #editToolBar.addWidget(self.undoButton)
+        #editToolBar.addWidget(self.redoButton)
 
         editToolBar.setMovable(False)
-       
-
-
-    #Creamos las acciones que se añaden a los menus
-    def _createActions(self):
-
-        self.newFile = QAction(QIcon("textEdit/resources/icons/newFile.png"),trans('New'), self, triggered = self.fileController.fileNew, shortcut = 'Ctrl+n')
-        self.newFile.setStatusTip(trans('New file'))
-
-        self.openFile = QAction(QIcon("textEdit/resources/icons/openFile.png"),trans('Open'), self, triggered = self.fileController.fileOpen, shortcut = 'Ctrl+a')
-        self.openFile.setStatusTip(trans('Open file'))
-
-        self.saveFile = QAction(QIcon("textEdit/resources/icons/saveFile.png"),trans('Save'), self, triggered = self.fileController.fileSave, shortcut = 'Ctrl+s')
-        self.saveFile.setStatusTip(trans('Save changes'))
-
-        self.closeApp = QAction('Close', self, triggered = self.close, shortcut = 'Ctrl+q')
-        self.closeApp.setStatusTip(trans('Close app'))
-
-        self.copyText = QAction(QIcon("textEdit/resources/icons/copy.png"),trans('Copy'), self, triggered = self.tools.textCopy, shortcut = 'Ctrl+c')
-        self.copyText.setStatusTip(trans('Copy selected text'))
-
-        self.pasteText = QAction(QIcon("textEdit/resources/icons/paste.png"),trans('Paste'), self, triggered = self.tools.textPaste, shortcut = 'Ctrl+v')
-        self.pasteText.setStatusTip(trans('Paste from clipboard'))
-
-        self.cutText = QAction(QIcon("textEdit/resources/icons/cut.png"),trans('Cut'), self, triggered = self.tools.textCut, shortcut = 'Ctrl+x')
-        self.cutText.setStatusTip(trans('Cut selected text'))
-
-        self.undoStack.indexChanged.connect(self.fileController.writeFile)
-
-        self.undo = self.undoStack.createUndoAction(self, self.tr('Undo'))
-        self.undo.setIcon(QIcon('textEdit/resources/icons/undo.png'))
-        self.undo.setShortcut('Ctrl+z')
-        #self.undo = QAction(QIcon("resources/icons/undo.png"), 'Undo', self, shortcut = 'Ctrl+z')
-        self.undo.setStatusTip('Undo')
-
-        self.undoButton = QToolButton()
-        self.undoButton.setText(trans('Undo'))
-        self.undoButton.setToolTip(trans('Undo'))
-        self.undoButton.setDefaultAction(self.undo)
-        self.undoButton.setEnabled(True)
-        self.undoButton.setIcon(QIcon('textEdit/resources/icons/undo.png'))
-        
-        self.redo = self.undoStack.createRedoAction(self, self.tr('Redo'))
-        self.redo.setIcon(QIcon('textEdit/resources/icons/redo.png'))
-        self.redo.setShortcut('Ctrl+y')
-        #self.redo = QAction(QIcon("resources/icons/redo.png"), 'Redo', self, shortcut = 'Ctrl+y')
-        self.redo.setStatusTip('Redo')
-        #self.redo.triggered.connect(QUndoCommand.redo())
-
-        self.redoButton = QToolButton()
-        self.redoButton.setText(trans('Redo'))
-        self.redoButton.setToolTip(trans('Redo'))
-        self.redoButton.setDefaultAction(self.redo)
-        self.redoButton.setEnabled(True)
-        self.redoButton.setIcon(QIcon('textEdit/resources/icons/redo.png'))
-
-        self.header1 = QAction(QIcon("textEdit/resources/icons/header1.png"), trans('Header 1'), self, triggered = self.tools.textH1, shortcut='Ctrl+h+1')
-        self.header1.setStatusTip(trans('Header 1'))
-
-        self.header2 = QAction(QIcon("textEdit/resources/icons/header2.png"), trans('Header 2'), self, triggered = self.tools.textH2, shortcut='Ctrl+h+2')
-        self.header2.setStatusTip(trans('Header 2'))
-
-        self.header3 = QAction(QIcon("textEdit/resources/icons/header3.png"), trans('Header 3'), self, triggered = self.tools.textH3, shortcut='Ctrl+h+3')
-        self.header3.setStatusTip(trans('Header 3'))
-
-        self.bold = QAction(QIcon("textEdit/resources/icons/bold.png"), trans('Bold'), self, triggered = self.tools.textBold, shortcut = 'Ctrl+n')
-        self.bold.setStatusTip(trans('Bold text'))
-
-        self.italic = QAction(QIcon("textEdit/resources/icons/italic.png"), trans('Italic'), self, triggered = self.tools.textItalic, shortcut = 'Ctrl+k')
-        self.italic.setStatusTip(trans('Italic text'))
-
-        self.help = QAction(trans('Help'), self)
-        self.help.setShortcut('Ctrl+h')
-        self.help.setStatusTip(trans('Help'))
-
-        self.about = QAction(trans('About TextEdit'), self)
-        self.about.setStatusTip(trans('About TextEdit'))
 
     #Función cerrar que pregunta al usuario si está seguro antes de hacerlo
     def close(self):
@@ -201,57 +122,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if choice == QMessageBox.Si:
             qApp.quit
         else: pass
-
-    #Función copiar
-    def textCopy(self):
-        tc = self.textEdit.textCursor()
-        text = QTextCursor.selectedText(tc)
-        self.clipboard.setText(text)
-
-    #Función pegar
-    def textPaste(self):
-        tc = self.textEdit.textCursor()
-        if QTextCursor.selectedText(tc) != '':
-            QTextCursor.removeSelectedText(tc)
-        tc.insertText(self.clipboard.text())
-    
-    #Función cortar
-    def textCut(self):
-        tc = self.textEdit.textCursor()
-        text = QTextCursor.selectedText(tc)
-        self.clipboard.setText(text)
-        QTextCursor.removeSelectedText(tc)
-
-    #Función negrita
-    def textBold(self):
-        tc = self.textEdit.textCursor()
-        if QTextCursor.selectedText(tc) != '':
-            text = '**' + QTextCursor.selectedText(tc) + '**'
-            tc.insertText(text)
-
-    #Función cursiva
-    def textItalic(self):
-        tc = self.textEdit.textCursor()
-        if QTextCursor.selectedText(tc) != '':
-            tc.insertText('*' + QTextCursor.selectedText(tc) + '*')
-
-    #Función h1
-    def textH1(self):
-        tc = self.textEdit.textCursor()
-        if QTextCursor.selectedText(tc) != '':
-            tc.insertText('# ' + QTextCursor.selectedText(tc))
-
-    #Función h2
-    def textH2(self):
-        tc = self.textEdit.textCursor()
-        if QTextCursor.selectedText(tc) != '':
-            tc.insertText('## ' + QTextCursor.selectedText(tc))  
-
-    #Función h3
-    def textH3(self):
-        tc = self.textEdit.textCursor()
-        if QTextCursor.selectedText(tc) != '':
-            tc.insertText('### ' + QTextCursor.selectedText(tc))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- textEdit/utils/widgets/actions.py
+++ textEdit/utils/widgets/actions.py
@@ -4,35 +4,61 @@ from PyQt5.QtGui import QIcon
 
 from ..i18n import trans
 from controllers.file import FileController
-from controllers.textEditingTools import TextEditingTools
+from controllers.text import TextEditingTools
 
 class Actions:        
 
-    #Creamos las acciones que se añaden a los menus
-    def createActions(self, app):
+    def __init__(self, app):
         self.app = app
         self.tools = TextEditingTools(self.app)
         self.fileController = FileController(self.app)
-        
-        self.newFile = QAction(QIcon("textEdit/resources/icons/newFile.png"),trans('New'), self, triggered = self.fileController.fileNew, shortcut = 'Ctrl+n')
+
+        self.newFile = QAction()
+        self.newFile.setIcon(QIcon("textEdit/resources/icons/newFile.png"))
+        self.newFile.setText(trans('New'))
+        self.newFile.setShortcut('Ctrl+n')
+        self.newFile.triggered.connect(self.fileController.fileNew)
         self.newFile.setStatusTip(trans('New file'))
 
-        self.openFile = QAction(QIcon("textEdit/resources/icons/openFile.png"),trans('Open'), self, triggered = self.fileController.fileOpen, shortcut = 'Ctrl+a')
+        self.openFile = QAction()
+        self.openFile.setIcon(QIcon("textEdit/resources/icons/openFile.png"))
+        self.openFile.setText(trans('Open'))
+        self.openFile.setShortcut('Ctrl+a')
+        self.openFile.triggered.connect(self.fileController.fileOpen)
         self.openFile.setStatusTip(trans('Open file'))
 
-        self.saveFile = QAction(QIcon("textEdit/resources/icons/saveFile.png"),trans('Save'), self, triggered = self.fileController.fileSave, shortcut = 'Ctrl+s')
+        self.saveFile = QAction()
+        self.saveFile.setIcon(QIcon("textEdit/resources/icons/saveFile.png"))
+        self.saveFile.setText(trans('Save'))
+        self.saveFile.setShortcut('Ctrl+s')
+        self.openFile.triggered.connect(self.fileController.fileSave)
         self.saveFile.setStatusTip(trans('Save changes'))
 
-        self.closeApp = QAction('Close', self, triggered = self.close, shortcut = 'Ctrl+q')
+        self.closeApp = QAction()
+        self.closeApp.setText(trans('Close'))
+        self.closeApp.setShortcut('Ctrl+q')
+        self.closeApp.triggered.connect(self.app.close)
         self.closeApp.setStatusTip(trans('Close app'))
 
-        self.copyText = QAction(QIcon("textEdit/resources/icons/copy.png"),trans('Copy'), self, triggered = self.tools.textCopy, shortcut = 'Ctrl+c')
+        self.copyText = QAction()
+        self.copyText.setIcon(QIcon("textEdit/resources/icons/copy.png"))
+        self.copyText.setText(trans('Copy'))
+        self.copyText.setShortcut('Ctrl+c')
+        self.copyText.triggered.connect(self.tools.textCopy)
         self.copyText.setStatusTip(trans('Copy selected text'))
 
-        self.pasteText = QAction(QIcon("textEdit/resources/icons/paste.png"),trans('Paste'), self, triggered = self.tools.textPaste, shortcut = 'Ctrl+v')
+        self.pasteText = QAction()
+        self.pasteText.setIcon(QIcon("textEdit/resources/icons/paste.png"))
+        self.pasteText.setText(trans('Paste'))
+        self.pasteText.setShortcut('Ctrl+v')
+        self.pasteText.triggered.connect(self.tools.textPaste)
         self.pasteText.setStatusTip(trans('Paste from clipboard'))
 
-        self.cutText = QAction(QIcon("textEdit/resources/icons/cut.png"),trans('Cut'), self, triggered = self.tools.textCut, shortcut = 'Ctrl+x')
+        self.cutText = QAction()
+        self.cutText.setIcon(QIcon("textEdit/resources/icons/cut.png"))
+        self.cutText.setText(trans('Cut'))
+        self.cutText.setShortcut('Ctrl+x')
+        self.cutText.triggered.connect(self.tools.textCut)
         self.cutText.setStatusTip(trans('Cut selected text'))
 
         '''
@@ -68,24 +94,46 @@ class Actions:
 
         '''
 
-        self.header1 = QAction(QIcon("textEdit/resources/icons/header1.png"), trans('Header 1'), self, triggered = self.tools.textH1, shortcut='Ctrl+h+1')
+        self.header1 = QAction()
+        self.header1.setIcon(QIcon("textEdit/resources/icons/header1.png"))
+        self.header1.setText(trans('Header 1'))
+        self.header1.setShortcut('Ctrl+h+1')
+        self.header1.triggered.connect(self.tools.textH1)
         self.header1.setStatusTip(trans('Header 1'))
 
-        self.header2 = QAction(QIcon("textEdit/resources/icons/header2.png"), trans('Header 2'), self, triggered = self.tools.textH2, shortcut='Ctrl+h+2')
+        self.header2 = QAction()
+        self.header2.setIcon(QIcon("textEdit/resources/icons/header2.png"))
+        self.header2.setText(trans('Header 2'))
+        self.header2.setShortcut('Ctrl+h+2')
+        self.header2.triggered.connect(self.tools.textH2)
         self.header2.setStatusTip(trans('Header 2'))
 
-        self.header3 = QAction(QIcon("textEdit/resources/icons/header3.png"), trans('Header 3'), self, triggered = self.tools.textH3, shortcut='Ctrl+h+3')
+        self.header3 = QAction()
+        self.header3.setIcon(QIcon("textEdit/resources/icons/header3.png"))
+        self.header3.setText(trans('Header 3'))
+        self.header3.setShortcut('Ctrl+h+3')
+        self.header3.triggered.connect(self.tools.textH3)
         self.header3.setStatusTip(trans('Header 3'))
 
-        self.bold = QAction(QIcon("textEdit/resources/icons/bold.png"), trans('Bold'), self, triggered = self.tools.textBold, shortcut = 'Ctrl+n')
+        self.bold = QAction()
+        self.bold.setIcon(QIcon("textEdit/resources/icons/bold.png"))
+        self.bold.setText(trans('Bold'))
+        self.bold.setShortcut('Ctrl+n')
+        self.bold.triggered.connect(self.tools.textBold)
         self.bold.setStatusTip(trans('Bold text'))
 
-        self.italic = QAction(QIcon("textEdit/resources/icons/italic.png"), trans('Italic'), self, triggered = self.tools.textItalic, shortcut = 'Ctrl+k')
+        self.italic = QAction()
+        self.italic.setIcon(QIcon("textEdit/resources/icons/italic.png"))
+        self.italic.setText(trans('Italic'))
+        self.italic.setShortcut('Ctrl+k')
+        self.italic.triggered.connect(self.tools.textItalic)
         self.italic.setStatusTip(trans('Italic text'))
 
-        self.help = QAction(trans('Help'), self)
+        self.help = QAction()
+        self.help.setText(trans('Help'))
         self.help.setShortcut('Ctrl+h')
         self.help.setStatusTip(trans('Help'))
 
-        self.about = QAction(trans('About TextEdit'), self)
+        self.about = QAction()
+        self.about.setText(trans('About...'))
         self.about.setStatusTip(trans('About TextEdit'))
